7020724585ba0b7ddf14aebd284285d553fffc3d
kaze50 <176711389+kaze50@users.noreply.github.com>
2024-08-22 09:23:49 +0900

    remove unused files

--- test.sh
+++
@@ -1,7 +0,0 @@
-python3 tester.py
-
-echo
-echo "./tester"
-echo "------------------------"
-./tester
-echo "------------------------"
--- tester.c
+++
@@ -1,7 +0,0 @@
-#include "include/metro.h"
-
-int main() {
-  mt_object* obj = mt_obj_new_int(123);
-
-  printf("%ld\n", obj->vi);
-}
